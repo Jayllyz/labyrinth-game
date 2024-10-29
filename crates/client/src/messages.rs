a32d3682e57e956fdc593d2bbaa773a0e462f437
Antony David <antonydavid945@gmail.com>
2024-10-29 21:22:16 +0100

    refactor: rmove useless tests

--- crates/client/src/messages.rs
+++ crates/client/src/messages.rs
@@ -96,58 +96,7 @@ mod tests {
     }
 
     #[test]
-    fn test_welcome_message() {
-        let welcome = Welcome { version: 1 };
-        let msg = Message::Welcome(welcome);
-        let serialized = serde_json::to_string(&msg).unwrap();
-        assert_eq!(serialized, r#"{"Welcome":{"version":1}}"#);
-
-        let deserialized: Message = serde_json::from_str(&serialized).unwrap();
-        matches!(deserialized, Message::Welcome(_));
-    }
-
-    #[test]
-    fn test_subscribe_message() {
-        let subscribe = Subscribe { name: "Player1".to_string() };
-        let msg = Message::Subscribe(subscribe);
-        let serialized = serde_json::to_string(&msg).unwrap();
-        assert_eq!(serialized, r#"{"Subscribe":{"name":"Player1"}}"#);
-    }
-
-    #[test]
-    fn test_view_message() {
-        let view = View { view: "game state".to_string() };
-        let msg = Message::View(view);
-        let serialized = serde_json::to_string(&msg).unwrap();
-        assert_eq!(serialized, r#"{"View":{"view":"game state"}}"#);
-    }
-
-    #[test]
-    fn test_action_message() {
-        let action = Action::MoveTo(Direction::Right);
-        let msg = Message::Action(action);
-        let serialized = serde_json::to_string(&msg).unwrap();
-        assert_eq!(serialized, r#"{"Action":{"MoveTo":"Right"}}"#);
-    }
-
-    #[test]
-    fn test_action_result() {
-        let results = vec![
-            ActionResult::Ok,
-            ActionResult::Completed,
-            ActionResult::Err(ActionError::InvalidMove),
-        ];
-
-        for result in results {
-            let msg = Message::ActionResult(result);
-            let serialized = serde_json::to_string(&msg).unwrap();
-            let deserialized: Message = serde_json::from_str(&serialized).unwrap();
-            matches!(deserialized, Message::ActionResult(_));
-        }
-    }
-
-    #[test]
-    fn test_full_message_flow() {
+    fn test_all_messages() {
         let messages = vec![
             Message::Hello,
             Message::Welcome(Welcome { version: 1 }),
