91d6f87d7e914f73808de610b3a0adf577339843
Metololo <93775692+Metololo@users.noreply.github.com>
2024-11-03 18:41:50 +0100

    feat: remove g and h parameter from priority queue

--- crates/client/src/data_structures/priority_queue.rs
+++ crates/client/src/data_structures/priority_queue.rs
@@ -2,23 +2,17 @@
 
 use crate::maze::Cell;
 use std::cmp::{Ord, Ordering};
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashSet};
 
 #[derive(PartialEq, Eq)]
 pub struct Node {
-    f: i32,
-    g: i32,
-    h: i32,
-    cell: Cell,
+    pub priority_f: i32,
+    pub cell: Cell,
 }
 
 impl Ord for Node {
     fn cmp(&self, other: &Node) -> Ordering {
-        self.f
-            .cmp(&other.f)
-            .then_with(|| self.g.cmp(&other.g))
-            .then_with(|| self.h.cmp(&other.h))
-            .reverse()
+        self.priority_f.cmp(&other.priority_f).reverse()
     }
 }
 
@@ -30,11 +24,12 @@ impl PartialOrd for Node {
 #[derive(Default)]
 pub struct PriorityQueue {
     heap: BinaryHeap<Node>,
+    cells: HashSet<Cell>,
 }
 
 impl PriorityQueue {
     pub fn new() -> Self {
-        Self { heap: BinaryHeap::new() }
+        Self { heap: BinaryHeap::new(), cells: HashSet::new() }
     }
 
     pub fn is_empty(&self) -> bool {
@@ -42,11 +37,18 @@ impl PriorityQueue {
     }
 
     pub fn enqueue(&mut self, node: Node) {
+        self.cells.insert(node.cell);
         self.heap.push(node);
     }
 
     pub fn dequeue(&mut self) -> Node {
-        self.heap.pop().expect("empty queue")
+        let n: Node = self.heap.pop().expect("empty queue");
+        self.cells.remove(&n.cell);
+        n
+    }
+
+    pub fn contains(&self, target_cell: &Cell) -> bool {
+        self.cells.contains(target_cell)
     }
 }
 
@@ -65,40 +67,36 @@ mod test {
     fn test_enqueue_and_dequeue_single_node() {
         let mut priority_queue = PriorityQueue::new();
 
-        let node = Node { f: 1, g: 2, h: 3, cell: Cell { row: 0, column: 0 } };
+        let node = Node { priority_f: 1, cell: Cell { row: 0, column: 0 } };
 
         priority_queue.enqueue(node);
 
         assert_eq!(priority_queue.is_empty(), false);
 
         let dequeued_node = priority_queue.dequeue();
-        assert_eq!(dequeued_node.f, 1);
-        assert_eq!(dequeued_node.g, 2);
-        assert_eq!(dequeued_node.h, 3);
+        assert_eq!(dequeued_node.priority_f, 1);
     }
 
     #[test]
     fn test_enqueue_and_dequeue_multiple_nodes() {
         let mut priority_queue = PriorityQueue::new();
 
-        let node1 = Node { f: 3, g: 2, h: 1, cell: Cell { row: 1, column: 1 } };
-        let node2 = Node { f: 1, g: 4, h: 3, cell: Cell { row: 2, column: 2 } };
-        let node3 = Node { f: 2, g: 3, h: 4, cell: Cell { row: 3, column: 3 } };
+        let node1 = Node { priority_f: 3, cell: Cell { row: 1, column: 1 } };
+        let node2 = Node { priority_f: 1, cell: Cell { row: 2, column: 2 } };
+        let node3 = Node { priority_f: 2, cell: Cell { row: 3, column: 3 } };
 
         priority_queue.enqueue(node1);
         priority_queue.enqueue(node2);
         priority_queue.enqueue(node3);
 
         let dequeued_node = priority_queue.dequeue();
-        assert_eq!(dequeued_node.f, 1);
-        assert_eq!(dequeued_node.g, 4);
-        assert_eq!(dequeued_node.h, 3);
+        assert_eq!(dequeued_node.priority_f, 1);
 
         let dequeued_node = priority_queue.dequeue();
-        assert_eq!(dequeued_node.f, 2);
+        assert_eq!(dequeued_node.priority_f, 2);
 
         let dequeued_node = priority_queue.dequeue();
-        assert_eq!(dequeued_node.f, 3);
+        assert_eq!(dequeued_node.priority_f, 3);
 
         assert_eq!(priority_queue.is_empty(), true);
     }
@@ -107,20 +105,20 @@ mod test {
     fn test_priority_with_same_values() {
         let mut priority_queue = PriorityQueue::new();
 
-        let node1 = Node { f: 3, g: 2, h: 1, cell: Cell { row: 1, column: 1 } };
-        let node2 = Node { f: 3, g: 4, h: 1, cell: Cell { row: 2, column: 2 } };
-        let node3 = Node { f: 3, g: 4, h: 2, cell: Cell { row: 3, column: 3 } };
+        let node1 = Node { priority_f: 3, cell: Cell { row: 1, column: 1 } };
+        let node2 = Node { priority_f: 3, cell: Cell { row: 2, column: 2 } };
+        let node3 = Node { priority_f: 2, cell: Cell { row: 3, column: 3 } };
 
         priority_queue.enqueue(node2);
         priority_queue.enqueue(node3);
         priority_queue.enqueue(node1);
 
         let dequeued_node = priority_queue.dequeue();
-        assert_eq!(dequeued_node.cell, Cell { row: 1, column: 1 });
+        assert_eq!(dequeued_node.priority_f, 2);
         let dequeued_node = priority_queue.dequeue();
-        assert_eq!(dequeued_node.cell, Cell { row: 2, column: 2 });
+        assert_eq!(dequeued_node.priority_f, 3);
         let dequeued_node = priority_queue.dequeue();
-        assert_eq!(dequeued_node.cell, Cell { row: 3, column: 3 });
+        assert_eq!(dequeued_node.priority_f, 3);
 
         assert_eq!(priority_queue.is_empty(), true);
     }
@@ -131,5 +129,21 @@ mod test {
         let mut priority_queue = PriorityQueue::new();
 
         priority_queue.dequeue();
+    }
+
+    #[test]
+    fn test_contain_cell() {
+        let mut priority_queue = PriorityQueue::new();
+
+        let node1 = Node { priority_f: 3, cell: Cell { row: 43, column: 12 } };
+        priority_queue.enqueue(node1);
+
+        assert_eq!(priority_queue.contains(&Cell { row: 43, column: 12 }), true);
+
+        priority_queue.dequeue();
+
+        assert_eq!(priority_queue.contains(&Cell { row: 43, column: 12 }), false);
+
+        assert_eq!(priority_queue.contains(&Cell { row: 4, column: 4 }), false);
     }
 }
