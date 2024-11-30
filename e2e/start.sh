fbf4c06265f62e90569f2fd80fb85d3148961bb8
Antony David <antonydavid945@gmail.com>
2024-11-30 10:34:06 +0100

    test: setup docker for e2e workflow

---
+++ e2e/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create XDG runtime directory
+mkdir -p /tmp/runtime-dir
+chmod 700 /tmp/runtime-dir
+
+# Start virtual X server
+Xvfb :99 -screen 0 1024x768x24 &
+sleep 1
+
+# Start the server in background
+/usr/local/bin/server run &
+sleep 5
+
+# Start the client
+RUST_BACKTRACE=1 /usr/local/bin/client
